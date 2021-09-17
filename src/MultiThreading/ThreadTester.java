39622f675a0a15f2b7a7d188056e071ed6191eae
Zailta <mannanbhat60@gmail.com>
2021-09-17 23:27:44 +0530

    MultiThreading using Thread Class

--- src/MultiThreading/ThreadTester.java
+++ src/MultiThreading/ThreadTester.java
@@ -2,7 +2,9 @@ package MultiThreading;
 
 public class ThreadTester {
     public static void main(String[] args) {
+        // creating the object of Counter class
         Counter object = new Counter();
+       // Creating class treads
         ThreadClass1 t1 = new ThreadClass1(object);
         ThreadClass2 t2 = new ThreadClass2(object);
         t2.start();
